74ba6492a762b1f1876a4b5ca37f533d453a3e53
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-11-05 21:29:15 +0900

    add permission and dependencies

--- app/src/main/java/jp/syukai/runnersannounce/MainActivity.kt
+++ app/src/main/java/jp/syukai/runnersannounce/MainActivity.kt
@@ -1,19 +1,32 @@
 package jp.syukai.runnersannounce
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.Manifest
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_PERMISSION = 10
+    private lateinit var fusedLocationClient: FusedLocationProviderClient;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -34,6 +47,61 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+    // 位置情報許可の確認
+    fun checkPermission() {
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)==
+            PackageManager.PERMISSION_GRANTED){
+
+            locationActivity();
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // Intent でLocation
+    fun locationActivity() {
+
+//        fusedLocationClient.lastLocation
+//            .addOnSuccessListener { location : Location? ->
+//                System.out.println(location.toString())
+//                // Got last known location. In some rare situations this can be null.
+//            }
+//        Intent intent = new Intent(getApplication(), LocationActivity.class);
+//        startActivity(intent);
+    }
+
+    // 許可を求める
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_PERMISSION
+            )
+
+        } else {
+            val toast = Toast.makeText(
+                this,
+                "許可されないとアプリが実行できません", Toast.LENGTH_SHORT
+            )
+            toast.show()
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_PERMISSION
+            )
+
         }
     }
 }
